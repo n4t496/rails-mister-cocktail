274cb2c1c43c068cb0c987b662ae8fe59db0efd6
Natasha Darlington <natasha.darlington@btinternet.com>
2018-11-09 12:57:07 +0000

    Mister Cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails
-
+  resources :cocktails, except: [:destroy, :edit, :update]
+  # get 'ingredients', to: 'ingredients#index'
+  # get 'cocktails', to: 'cocktails#index'
+  # get 'cocktail/new', to: 'cocktails#new'
+  # post 'cocktail', to: 'cocktails#create'
+  # get 'cocktail/:id', to: 'cocktails#show'
 end
