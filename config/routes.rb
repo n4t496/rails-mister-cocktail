1f84adb605f26fdf011346154667d7252aab9362
Natasha Darlington <natasha.darlington@btinternet.com>
2018-11-09 10:34:57 +0000

    Mister Cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :cocktails, only: [ :index, :show, :new, :create ] do
-  #   resources :doses, only: [ :new, :create ]
-  # end
-  # delete '/doses/:id', to: 'doses#destroy', as: 'dose'
-  # root 'cocktails#index'
-  # mount Attachinary::Engine => "/attachinary"
+  resources :cocktails
+
 end
