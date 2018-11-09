f2d3be5fd722c497237689d898b7865808b029f5
Natasha Darlington <natasha.darlington@btinternet.com>
2018-11-09 14:40:03 +0000

    fghj

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles, except: :index
-
-  # resources :articles
-  # resources :cocktails, except: [:destroy, :edit, :update]
+  resources :articles
+  resources :cocktails, except: [:destroy, :edit, :update]
   # get 'ingredients', to: 'ingredients#index'
   # get 'cocktails', to: 'cocktails#index'
   # get 'cocktail/new', to: 'cocktails#new'
